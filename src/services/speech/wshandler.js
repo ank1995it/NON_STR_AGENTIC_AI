27bef20af3cf9e7cc631a0497da4825c2ba03f89
Ankit Nautiyal <43695208+ank1995it@users.noreply.github.com>
2026-02-03 16:44:04 +0530

    Update wshandler.js

--- src/services/speech/wshandler.js
+++ src/services/speech/wshandler.js
@@ -7,15 +7,13 @@ import { TwiMLTTSService } from "../tts/twiml.js";
 import { TwilioAudioStreamer } from "../background/index.js";
 import AsyncLock from "async-lock";
 import { performance } from "node:perf_hooks";
-import { error } from "node:console";
-import {CallStreamManager} from "../grpc/grpc-client.js"
-
-
+import { CallStreamManager } from "../grpc/grpc-client.js";
+import { CallEventPublisher } from "../snsEvents/callEventPublisher.js";
 export class WebSocketHandler {
   constructor(socket, speechManager, services, callId, llmUrl, logger) {
     this.llmUrl = llmUrl;
     this.callId = callId;
-    this.logger = logger.child({service: 'WebSocketHandler'})
+    this.logger = logger.child({ service: "WebSocketHandler" });
     this.socket = socket;
     this.speechManager = speechManager;
     this.services = services;
@@ -33,9 +31,16 @@ export class WebSocketHandler {
     this.isSpeechStartDetected = false;
     this.speechStartTime = 0;
     this.speechProcessTime = 0;
-    this.callStreamManager = new CallStreamManager(this.callId, this.logger)
-    //this.audioStreamer = null; //due to distortioncommented on 27-03-25
+    this.callStreamManager = new CallStreamManager(this.callId, this.logger);
+    this.audioStreamer = null; //due to distortioncommented on 27-03-25
+    this.bgsound = true;
+    this.callStartedEventSent = false;
+    this.callConnectedEventSent = false;
+    this.callEndedEventSent = false;
+    this.lastUserUtterance = null;
+    this.postCallEventSent = false;
 
+    this.eventPublisher = new CallEventPublisher(callId, this.logger);
     // Mark and Clear event handling properties
     this.markQueue = [];
 
@@ -44,7 +49,6 @@ export class WebSocketHandler {
     this.lock = new AsyncLock(); // lock object created for async operations
 
     this.silenceDetector.on("silenceWarning", async (message) => {
-      //this.audioStreamer.stop(); // added on 27-03-25
       await this.handleTranscriptProcessing(""); // added on 29-03-25
       if (!this.speechManager.isSpeaking()) {
         this.fetchRedisData = true;
@@ -54,7 +58,7 @@ export class WebSocketHandler {
     });
 
     this.silenceDetector.on("welcomeMsg", async (message) => {
-       this.logger.info(
+      this.logger.info(
         "===================Initiating Conversation========================="
       );
       if (!this.speechManager.isSpeaking()) {
@@ -68,7 +72,7 @@ export class WebSocketHandler {
       this.handleStop();
     });
 
-     this.logger.info("WebSocketHandler initialized");
+    this.logger.info("WebSocketHandler initialized");
   }
 
   getTimestamp() {
@@ -76,7 +80,9 @@ export class WebSocketHandler {
   }
 
   setupEventHandlers() {
-     this.logger.info(`[${this.getTimestamp()}] Setting up WebSocket event handlers`);
+    this.logger.info(
+      `[${this.getTimestamp()}] Setting up WebSocket event handlers`
+    );
     this.socket.on("message", async (rawData) => {
       try {
         let data;
@@ -92,8 +98,8 @@ export class WebSocketHandler {
 
         const msg = JSON.parse(data);
         await this.handleMessage(msg);
-      } catch (error) {        
-         this.logger.error("Error processing WebSocket message:", {
+      } catch (error) {
+        this.logger.error("Error processing WebSocket message:", {
           error: error.message,
           dataType: typeof rawData,
           isBuffer: Buffer.isBuffer(rawData),
@@ -129,27 +135,38 @@ export class WebSocketHandler {
           this.handleMarkEvent(msg);
           break;
         default:
-          this.logger.error({error : msg.event},
-            `Unknown message event:`
-          );
+          this.logger.error({ error: msg.event }, `Unknown message event:`);
       }
     } catch (error) {
       console.error(`[${this.getTimestamp()}] Message handling error:`, error);
-       this.logger.error({
-        error: error.message,
-        event: msg?.event,
-      },"Message handling error:");
+      this.logger.error(
+        {
+          error: error.message,
+          event: msg?.event,
+        },
+        "Message handling error:"
+      );
     }
   }
 
   async handleStart(msg) {
     try {
-       this.logger.info(JSON.stringify(msg));
+      this.logger.info(JSON.stringify(msg));
       const { streamSid } = msg.start;
       const callSid = this.callId;
       this.speechManager.setStreamSidAndcallSid(streamSid, callSid);
-      this.callStreamManager.initStream()
-      //this.audioStreamer = new TwilioAudioStreamer(this.markQueue, this.socket, streamSid);//due to distortioncommented on 27-03-25
+
+      if (!this.callConnectedEventSent) {
+        this.eventPublisher.publishCallEvent("CALL_CONNECTED");
+
+        this.callConnectedEventSent = true;
+      }
+
+      this.audioStreamer = new TwilioAudioStreamer(
+        this.markQueue,
+        this.socket,
+        streamSid
+      ); //due to distortioncommented on 27-03-25
 
       this.markQueue = [];
 
@@ -162,8 +179,8 @@ export class WebSocketHandler {
           "utf-8"
         )
       );
-       this.logger.info({msg: sttJson.locale}, "STT Data decoded");
-       this.logger.info(Object.keys(this.services))
+      this.logger.info({ msg: sttJson.locale }, "STT Data decoded");
+      this.logger.info(Object.keys(this.services));
 
       const audioStream = this.services.audioTransformer.createReadableStream();
       //starting grpc stream here
@@ -175,7 +192,7 @@ export class WebSocketHandler {
       // Event handler for when a new speech segment is detected
       let speechDetectedLogged = false;
       recognizer.speechStartDetected = (_, event) => {
-         this.logger.info({
+        this.logger.info({
           msg: `SPEECH START DETECTED`,
           callSid,
         });
@@ -183,11 +200,11 @@ export class WebSocketHandler {
 
       // Event handler for when a speech segment has ended
       recognizer.speechEndDetected = (_, event) => {
-         this.logger.info({msg : JSON.stringify(event)});
+        this.logger.info({ msg: JSON.stringify(event) });
       };
 
       // Set up Azure recognition events
-      recognizer.recognized = (_, event) => {
+      recognizer.recognized = async (_, event) => {
         //console.log(event.result.reason);
         //console.log(sdk.ResultReason.RecognizedSpeech);
         const endTime = performance.now();
@@ -197,7 +214,7 @@ export class WebSocketHandler {
           if (speechDetectedLogged) {
             //  this.logger.info({callId: this.callId},{
             //   msg: `SPEECH LENGTH MS: ${endToEndLatency.toFixed(2)}`,callSid});
-             this.logger.info({
+            this.logger.info({
               msg: `SPEECH PROCESS [LATENCY], DURATION MS: ${processLatency.toFixed(
                 2
               )}`,
@@ -208,16 +225,25 @@ export class WebSocketHandler {
 
           const text = event.result.text;
           if (true) {
-            // if(this.audioStreamer)  this.audioStreamer.stop();
             this.isRecognizing = false; // added on 29-03-25
+            if (text && text.trim().length > 0) {
+              this.lastUserUtterance = text;
+
+              await this.eventPublisher.publishTranscript("USER_SPOKE", text, {
+                confidence: event.result.confidence,
+                locale: event.result.language,
+                speaker: "user",
+              });
+            }
             this.speechManager.handleTranscript(text, false);
             this.isMethodCalledOnce = false; // Reset method call tracker
+            this.bgsound = true;
           }
         }
       };
 
       recognizer.recognizing = (_, event) => {
-        this.logger.i
+        this.logger.i;
         this.isRecognizing = true; // added on 29-03-25
         this.ttsSilenceEmitted = false;
         this.fetchRedisData = false;
@@ -238,34 +264,26 @@ export class WebSocketHandler {
         ) {
           this.isSendInterruption = true; //added on 19-03-25
           this.isMethodCalledOnce = true; // Ensure this is called only once per recognition
-          queueMicrotask(() => {this.handleSpeechDetected();}); // Yield to allow TTS state to update          
-          
+          queueMicrotask(() => {
+            this.handleSpeechDetected();
+          }); // Yield to allow TTS state to update
         }
-        const url = `http://localhost:17000/partial?callid=${encodeURIComponent(this.callId)}`
-          fetch(url)
-          .then(res => {
-            this.logger.info( "response from partial api")
-            this.logger.info( res)
-          })
-          .catch(err => {
-            this.logger.error({
-              msg: "Partial API failed",
-              error: err.message
-            })
-          })
-
         //this.isTTSInterrupted = true;
-
-         this.logger.info({
+        this.logger.info({
           msg: "Partial transcription",
           callSid,
           text,
         });
         // this.logger.error({callId: this.callId},this.isRecognizing);
+        if (this.bgsound) {
+          this.audioStreamer.start();
+          this.logger.info("Background Audio Streamer Started");
+          this.bgsound = false;
+        }
       };
 
       recognizer.canceled = (_, event) => {
-         this.logger.error({
+        this.logger.error({
           msg: "Recognition canceled",
           callSid,
           reason: event.reason,
@@ -278,14 +296,14 @@ export class WebSocketHandler {
       };
 
       recognizer.sessionStopped = () => {
-         this.logger.info({
+        this.logger.info({
           msg: "Recognition session stopped",
           callSid,
         });
         this.cleanup();
       };
     } catch (error) {
-       this.logger.error(error,"Error in handleStart:");
+      this.logger.error(error, "Error in handleStart:");
       this.handleError(error);
     }
   }
@@ -293,40 +311,82 @@ export class WebSocketHandler {
   async handleTranscriptProcessing(transcript) {
     try {
       if (this.speechManager.isSpeaking() && transcript.length <= 5) {
-         this.logger.info("Skipping response generation - TTS in progress");
+        this.logger.info("Skipping response generation - TTS in progress");
         return;
       }
 
-       this.logger.info({ transcript }, "Generating agent response");
-      const response = await this.services.responseService.generateResponse(
+      this.logger.info({ transcript }, "Generating agent response");
+      const llmResponse = await this.services.responseService.generateResponse(
         transcript,
         this.speechManager.state.callSid,
         this.isSendInterruption,
         transcript === "" ? true : false,
         this.llmUrl
       );
+      const response = llmResponse?.assistant;
+      const cutCallStatus = llmResponse?.cut_call;
+
+      this.audioStreamer.stop(); //stop background noise
+      this.logger.info("Background Audio Streamer Stopping");
       this.isSendInterruption = false;
       if (response) {
         if (!this.ttsSilenceEmitted) {
           this.silenceDetector.handleActivity(); // reset silence detector
         }
-        //if (this.audioStreamer) this.audioStreamer.stop()//due to distortioncommented on 27-03-25
         await this.generateAndStreamTTS(response);
       }
+
+      if (cutCallStatus) {
+        new TwiMLTTSService(config).disconnectCall(this.callId);
+        this.handleStop();
+      }
     } catch (error) {
-       this.logger.error(error, "Response processing error");
+      this.logger.error(error, "Response processing error");
       this.speechManager.stopTTS();
+    }
+  }
+
+  async triggerPostCallSummary() {
+    try {
+      const url = new URL("http://localhost:17000/call_summary");
+      url.searchParams.append("callid", this.callId);
+      this.logger.info({ url }, "url for post call");
+
+      const response = await fetch(url.toString(), {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      return;
+
+      // const data = await response.json();
+      // this.logger.info({ data }, "data for post call");
+
+      // await this.eventPublisher.publishPostCallSummary(data);
+    } catch (error) {
+      this.logger.error(error, "Post call summary error");
     }
   }
 
   async generateAndStreamTTS(response) {
     try {
-       this.logger.info({ response }, "Starting TTS generation");
+      this.logger.info({ response }, "Starting TTS generation");
+      const ttsPayload =
+        typeof response === "string"
+          ? JSON.stringify({ assistant: response })
+          : JSON.stringify(response);
+      console.log("tts payload", ttsPayload);
+      this.logger.info({ ttsPayload }, ttsPayload);
+      await this.eventPublisher.publishTranscript("AIAGENT_SPOKE", ttsPayload, {
+        interrupted: this.isSendInterruption,
+        basedOn: this.lastUserUtterance,
+        speaker: "ai_agent",
+      });
       this.speechManager.startTTS();
       this.isTTSInterrupted = false;
       this.markQueue = [];
 
-      // if(this.audioStreamer)  this.audioStreamer.stop();//stop background noise
       if (this.isTTSLock) {
         this.isTTSInterrupted = true;
       }
@@ -339,14 +399,14 @@ export class WebSocketHandler {
       await this.lock.acquire(this.callId, async () => {
         this.isTTSLock = true; // Lock TTS
         const audioGenerator = await this.services.ttsService.synthesize(
-          response,
+          ttsPayload,
           this.callId
         );
 
         for await (const chunk of audioGenerator) {
           // Check for interruption
           if (this.isTTSInterrupted) {
-             this.logger.info("TTS interrupted, stopping audio stream");
+            this.logger.info("TTS interrupted, stopping audio stream");
             break;
           }
 
@@ -372,7 +432,7 @@ export class WebSocketHandler {
       );
       this.lastResponseStartTime = null;
     } catch (error) {
-       this.logger.error(error, "Error in TTS generation");
+      this.logger.error(error, "Error in TTS generation");
       this.speechManager.stopTTS();
       this.isTTSInterrupted = false;
       this.isAgentRecorderInterrupted = false;
@@ -380,7 +440,6 @@ export class WebSocketHandler {
       //this.speechManager.stopTTS();  we will stop TTS when we will receive event form twilio
       this.isTTSInterrupted = false;
       this.isAgentRecorderInterrupted = false;
-      // await this.audioStreamer.start();//due to distortioncommented on 27-03-25
       if (!this.ttsSilenceEmitted) {
         this.silenceDetector.handleActivity(); // reset silence detector
       }
@@ -447,17 +506,19 @@ export class WebSocketHandler {
     }
     if (msg.mark?.name === "endMark") {
       this.speechManager.stopTTS(); // Stop TTS if mark is received
-       this.logger.info({
+      this.logger.info({
         msg: "Processed mark event",
         markName: msg.mark?.name,
         remainingMarks: this.markQueue.length,
       });
+      this.audioStreamer.stop(); //stop background noise
+      this.logger.info("Background Audio Streamer Stopping");
     }
   }
 
   // Send clear event when speech is detected during TTS
   handleSpeechDetected() {
-     this.logger.info({
+    this.logger.info({
       msg: "Speech detected during TTS",
     });
 
@@ -476,18 +537,19 @@ export class WebSocketHandler {
     // Reset tracking
     this.markQueue = [];
 
-     this.logger.info({
+    this.logger.info({
       msg: "Sent clear event due to speech detection",
     });
   }
 
-  handleMedia(msg) {
+  async handleMedia(msg) {
     // if (!this.speechManager.isStreaming()) {
     //     console.warn(`[${this.getTimestamp()}] Received media before stream start`);
     //     //return;
     // }
 
     // Record caller audio
+
     if (msg.media?.payload && !this.isAgentRecorderInterrupted) {
       //this.services.audioRecorder.writeCallerAudio(Buffer.from(msg.media.payload, 'base64'));
     }
@@ -517,7 +579,9 @@ export class WebSocketHandler {
 
   async handleStop() {
     console.log(`[${this.getTimestamp()}] Handling stop event`);
-
+    queueMicrotask(() => {
+      this.triggerPostCallSummary();
+    });
     // Stop recording before cleanup
     //await this.services.audioRecorder.stopRecording();
     await this.cleanup();
@@ -545,10 +609,23 @@ export class WebSocketHandler {
       await this.eventHubClient.close();
     }
 
-    this.callStreamManager.close()
-    //this.audioStreamer = null;//due to distortioncommented on 27-03-25
+    this.callStreamManager.close();
+    this.audioStreamer = null; //due to distortioncommented on 27-03-25
     this.silenceDetector.cleanup();
+    await this.services.transcribeService.stopStream();
     this.speechManager.cleanup();
+    // await this.publishPostCallEvent();
     await this.services.cleanup?.();
+  }
+
+  async publishPostCallEvent() {
+    await this.eventPublisher.publishPostCallSummary({
+      durationSec: "To be calculated: 900 sec",
+      direction: "inbound",
+      summary: "Customer requested order cancellation",
+      sentiment: "neutral",
+      intent: "order_cancellation",
+      recordingUrl: "https://storage.blob.core.windows.net/recordings/1234.wav",
+    });
   }
 }
