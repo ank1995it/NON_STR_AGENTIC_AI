77cdcda47e63b63687693135f4956d1fd8bb8c42
Ankit Nautiyal <43695208+ank1995it@users.noreply.github.com>
2026-02-25 18:39:44 +0530

    Refactor TTSService constructor and logging

--- src/services/tts/index.js
+++ src/services/tts/index.js
@@ -3,7 +3,7 @@ import { config } from '../../config/index.js';
 import { PollyTTSService } from './polly.js';
 import { TwiMLTTSService } from './twiml.js';
 import { AzureTTSService } from './azure-speech.js';
-import { ElevenLabTtsService } from './eleven-labs.js';
+import { ElevenLabsTtsService } from './eleven-labs.js';
 import { OpenAzureTTSService } from './openai-azure-speech.js';
 import { DeepgramTtsService } from './deep-gram.js';
 
@@ -11,34 +11,34 @@ export class TTSService {
     constructor(ttsService, voiceId, logger) {
         this.logger= logger
         switch (ttsService || config.tts.provider.toLowerCase()) {
-            case 'polly':
-                this.logger.info('Using AWS Polly for TTS');
-                this.service = new PollyTTSService(voiceId, config);
-                break;
+            // case 'polly':
+            //     this.logger.info('Using AWS Polly for TTS');
+            //     this.service = new PollyTTSService(voiceId, config);
+            //     break;
             case 'elevenlabs':
                 this.logger.info('Using Eleven labs Eleven Labs for TTS');
-                this.service = new ElevenLabTtsService(voiceId,config);
+                this.service = new ElevenLabsTtsService(voiceId,config,logger);
                 break;
             case 'azure':
                 this.logger.info('Using Azure Speech for TTS');
-                this.service = new AzureTTSService(voiceId,config);
+                this.service = new AzureTTSService(voiceId,config,logger);
                 break;
-            case 'openazure':
-                this.logger.info('Using Azure Speech for TTS');
-                this.service = new OpenAzureTTSService(voiceId,config);
-                break;
-            case 'deepgram':            
-                this.logger.info('Using Deepgram for TTS');
-                this.service = new DeepgramTtsService(voiceId,config);
-                break;
+            // case 'openazure':
+            //     this.logger.info('Using Azure Speech for TTS');
+            //     this.service = new OpenAzureTTSService(voiceId,config);
+            //     break;
+            // case 'deepgram':            
+            //     this.logger.info('Using Deepgram for TTS');
+            //     this.service = new DeepgramTtsService(voiceId,config);
+            //     break;
             default:
                 this.logger.warn(`Unknown TTS provider: ${config.tts.provider}, defaulting to Eleven Labs`);
-                this.service = new ElevenLabTtsService(voiceId,config);
+                this.service = new ElevenLabsTtsService(voiceId,config,logger);
         }
     }
 
-    async synthesize(text, callId) {
-        return await this.service.synthesize(text, callId);
+     synthesize(text, callId) {
+        return  this.service.synthesize(text, callId);
     }
 }
 
