ab5f6e7ff729a2f1ff17503eedcf304c0455f91d
Ankit Nautiyal <43695208+ank1995it@users.noreply.github.com>
2026-02-25 16:32:51 +0530

    Update call-simulator.js

--- src/simulator/call-simulator.js
+++ src/simulator/call-simulator.js
@@ -1,21 +1,30 @@
 import WebSocket from 'ws';
 import { CONFIG } from './config.js';
 import { ConversationReplayEngine } from './turn.js';
+import { logger } from '../utils/logger.js';
 
 export function simulateCall(callNumber) {
   return new Promise((resolve, reject) => {
     const callId = `loadtest-${process.pid}-${Date.now()}-${callNumber}`;
     const streamSid = `stream-${callId}`;
+    const sttPayload  = {
+      locale:"en-US"
+    }
+
+    const sttBase64 = Buffer.from(JSON.stringify(sttPayload))
+    .toString("base64")
 
     const wsUrl =
       `ws://${CONFIG.HOST}:${CONFIG.PORT}` +
       `${CONFIG.WS_PREFIX}/${callId}/${CONFIG.VOICE_DATA}/${CONFIG.LLM_URL}`;
 
     const ws = new WebSocket(wsUrl);
-
+      const ttsActive = false;
+      let ttsTimeout;
     let seq = 1;   // MUST be declared before passing to engine
 
-    let replayEngine;  // declare first
+    /**@type {ConversationReplayEngine| null} */
+    let replayEngine= null;  // declare first
 
     ws.on('open', () => {
       console.log(`✅ WS connected: ${callId}`);
@@ -31,6 +40,9 @@ export function simulateCall(callNumber) {
             encoding: 'audio/x-mulaw',
             sampleRate: CONFIG.SAMPLE_RATE,
             channels: 1
+          },
+          customParameters: {
+            sttData: sttBase64
           }
         }
       }));
@@ -46,14 +58,36 @@ export function simulateCall(callNumber) {
       });
 
       replayEngine.start();
+
+
     });
 
     ws.on('message', (msg) => {
       try {
+        logger.info({event: data.event}, 'messag eevent inside simulator')
         const data = JSON.parse(msg);
+        logger.info(data.event)
+
+        if(data.event== 'media'){
+          ttsActive = true
+
+          clearTimeout(ttsTimeout);
+          ttsTimeout = setTimeout(()=>{
+               if(ttsActive){
+                ws.send(JSON.stringify({
+          event: 'mark',
+          sequenceNumber: seq++,
+          mark : {
+            name: `playback_complete_${this.current}`
+          }
+        }))
+        ttsActive= false;
+               }
+          }, 200)
+        }
 
         if (data.event === 'stop_tts') {
-          replayEngine?.handleTurnComplete();
+          replayEngine.handleTurnComplete();
         }
 
         if (data.event === 'agent_transcript') {
@@ -64,6 +98,7 @@ export function simulateCall(callNumber) {
         // Ignore non-JSON frames
       }
     });
+   
 
     ws.on('close', () => {
       console.log(`🔚 Call ended: ${callId}`);
